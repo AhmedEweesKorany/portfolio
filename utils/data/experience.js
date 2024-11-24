741acae8133c322b7b28cb4378aadb6e1a64637d
AhmedEweesKorany <ahmedewaisvi34@gmail.com>
2024-11-24 10:32:24 +0200

    first commit

---
+++ utils/data/experience.js
@@ -0,0 +1,15 @@
+export const experiences = [
+
+  {
+    id: 1,
+    title: "FullStack Developer",
+    company: "Webbrical",
+    duration: "(Aug 2022 - Sep 2024)"
+  },
+  {
+    id: 2,
+    title: "Self Employed",
+    company: "Code and build something in everyday.",
+    duration: "(Jan 2022 - Present)"
+  }
+]
