4dedf21267abf79e8e586138f0092e132da8d921
AhmedEweesKorany <ahmedewaisvi34@gmail.com>
2024-11-24 10:49:39 +0200

    fix ssr issue

--- app/page.js
+++ app/page.js
@@ -1,3 +1,6 @@
+'use client'; // Add this at the top of the file if you're in the `app` directory.
+
+import { useEffect } from "react";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
@@ -9,7 +12,13 @@ import Skills from "./components/homepage/skills";
 
 
 export default async function Home() {
-
+  useEffect(() => {
+    // Ensure this code only runs on the client side
+    if (typeof document !== "undefined") {
+      const element = document.getElementById("example");
+      console.log(element);
+    }
+  }, []);
   return (
     <>
       <HeroSection />
