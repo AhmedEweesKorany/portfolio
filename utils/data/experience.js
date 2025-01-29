28a5d388ce4818205b55639a6f6eb75a63418900
AhmedEweesKorany <ahmedewaisvi34@gmail.com>
2025-01-29 10:51:55 +0200

    add current job in experience section

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,13 +1,18 @@
 export const experiences = [
-
   {
     id: 1,
+    title: "Co Teacher",
+    company: "NASS Academy",
+    duration: "(Jan 2025 - present)"
+  },
+  {
+    id: 2,
     title: "FullStack Developer",
     company: "Webbrical",
     duration: "(Aug 2022 - Sep 2024)"
   },
   {
-    id: 2,
+    id: 3,
     title: "Self Employed",
     company: "Code and build something in everyday.",
     duration: "(Jan 2022 - Present)"
